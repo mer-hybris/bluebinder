0dbda9d70b846607891d0433013c848c4d35d1cd
Sergey Chupligin <neochapay@gmail.com>
2018-12-20 18:04:11 +0300

    [bluebinder] Allow use outside script for generate bt address file. Fixes MER#1998

--- bluebinder_wait.sh
+++ bluebinder_wait.sh
@@ -4,6 +4,11 @@ while true
 do
     bt_status=$(/usr/bin/getprop init.svc.bluetooth-1-0)
     if [ "$bt_status" = "running" ] ; then
+        # If the bluetooth address is provided by another script use that
+        if [ -f /var/lib/bluetooth/board-address ] ; then
+            exit 0
+        fi
+        # Getting address file from getprop
         bt_addr_file=$(/usr/bin/getprop ro.bt.bdaddr_path)
         if [ "$bt_addr_file" != "" ]; then
             mkdir -p /var/lib/bluetooth
