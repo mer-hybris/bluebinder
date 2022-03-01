63633d95dc8c052e0db43f94fbf16e78117e4410
Alfred E. Neumayer <dev.beidl@gmail.com>
2022-03-01 09:30:25 +0100

    wait: Check for versioned bluetooth service (#5)
    On the JingPad the vendor service on the Halium side has a version
    number appended on the service identifier.
    Accommodate for that to fix Bluebinder on the JingPad.

--- bluebinder_wait.sh
+++ bluebinder_wait.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    bt_status=$(/usr/bin/getprop |grep "init.svc.*bluetooth" |grep -o "\[running\]")
+    bt_status=$(/usr/bin/getprop |grep "init.svc.*bluetooth\|init.svc.*bluetooth-1-0" | grep -o "\[running\]" | uniq)
     if [ "$bt_status" = "[running]" ] ; then
         echo "Bluetooth service running"
         exit 0
